b9d8579958e9b2047e56cf455a145393f1ec7d16
Welly <whtp1490@gmail.com>
2021-01-13 20:27:22 -0800

    Update index.js

--- index.js
+++ index.js
@@ -34,22 +34,50 @@ const onInput = async event =>{
     //Find all movies from the input
     const movies = await fetchData(event.target.value);
     
+    //If nothing is in input, then dropdown does not show up
+    if(!movies.length){
+        dropdown.classList.remove('is-active');
+        return;
+    }
+
+    resultsWrapper.innerHTML = '';
     dropdown.classList.add('is-active');
 
     for(let movie of movies){
         const option = document.createElement('a');
+        const imgSRC = movie.Poster === 'N/A' ? '' : movie.Poster; 
 
         option.classList.add('dropdown-item');
         option.innerHTML = `
-            <img src="${movie.Poster}" />
+            <img src="${imgSRC}" />
             ${movie.Title}
         `;
+        option.addEventListener('click', () =>{
+            dropdown.classList.remove('is-active');
+            input.value = movie.Title;
+            onMovieSelect(movie);
+        });
 
         resultsWrapper.appendChild(option);
     }
 };
-input.addEventListener('input' ,debounce(onInput));
+input.addEventListener('input' ,debounce(onInput, 500));
+input.addEventListener('click', onInput);
 
+document.addEventListener('click', event => {
+    if(!root.contains(event.target)){
+        dropdown.classList.remove('is-active');
+    }
+});
+
+const onMovieSelect = async movie =>{
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params:{
+            apikey: 'b73304f0',
+            i: movie.imdbID
+        }
+    });
+}
 
 
 
